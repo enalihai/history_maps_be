afbee280c05877f08a67c0d5c604d5106b41d7bd
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-27 10:24:39 -0600

    Set up Factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,7 +64,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
 end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
