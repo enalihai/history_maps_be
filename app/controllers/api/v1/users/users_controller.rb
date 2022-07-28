d8495351cd271ff1fc43cc8205503f06249430ca
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-28 11:15:33 -0600

    Update Users Controller with new routes, need to test

---
+++ app/controllers/api/v1/users/users_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::Users::UsersController < ApplicationController
+
+  def index
+    render json: UserSerializer.new(User.all)
+  end
+
+  def show
+    user = User.find_by_id(params[:id])
+    if user
+      render json: UserSerializer.new(user)
+    else
+      render json: { status: "User Not Found", code: 404, message: "User Not Found" }, status: 404
+    end
+  end
+
+  def new; end
+
+  def create
+    user_create = User.create(user_params)
+    render json: UserSerializer.new(user_create), status: 201
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :username)
+    end
+end
