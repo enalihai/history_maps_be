4d19edb444fe2b7f7bdcc5f0ebca72cf8037bbd4
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-29 17:26:27 -0600

    A little bit of code cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,12 @@ Rails.application.routes.draw do
   # get '/'
 
   namespace :api do
-    namespace :v1 do
-      
+    namespace :v1 do      
       resources :users, only: [:index, :show] do
         resources :favorites, only: [:index, :show, :new, :create]
       end
-      # get '/users', to: 'users#index'
       get '/users/register', to: 'users#new'
       post '/users/register', to: 'users#create'
-      # get '/users/:id', to:'users#show'
-      # namespace :users do
-      #   # resources :register, only: [:new, :create], controller: :users
-      # end
-
-      # get '/favorites/new', to: 'favorites#new'
-      # post '/favorites/new', to: 'favorites#create'
 
       get '/search', to: 'location_search#show'
     end
