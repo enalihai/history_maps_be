9d1fe92c67aca40cf629842f2dcfb7484fba19dc
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-30 17:29:14 -0600

    Feat: LocService reaches a single item's endpoint from index results

--- app/services/loc_service.rb
+++ app/services/loc_service.rb
@@ -15,4 +15,14 @@ class LocService
     end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_single_location_data(location, result_index_id, item_id)
+    all_results = LocResultFacade.create_loc_index_results(location)
+    result = all_results.find { |item| item.index_id == result_index_id }
+    item_id = result.id
+    response = conn.get("pictures/collection/hh/item/#{item_id}/") do |faraday|
+      faraday.params['fo'] = 'json'
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
