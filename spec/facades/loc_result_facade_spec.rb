e0cf7fe55f0a91c508fb5aaaa62bc0fb7248e062
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-30 17:52:02 -0600

    Test: LoC single item creation for show

--- spec/facades/loc_result_facade_spec.rb
+++ spec/facades/loc_result_facade_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe LocResultFacade do
   it "gets index results from a location service" do
     results = LocResultFacade.create_loc_index_results('80033')
+    
     expect(results).to be_a Array
     expect(results).to be_all LocIndexResult
+  end
+
+  it "creates a single item object" do
+    result = LocResultFacade.create_single_item_result('80033', 1, 'co0994')
+
+    expect(result).to be_a LocItemResult
   end
 end
