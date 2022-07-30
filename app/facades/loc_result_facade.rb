555c0007daadac5204c8bf112252fd9162f222f3
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-30 17:52:54 -0600

    Feat: LoCResultFacade.create_single_item_result creates single item

--- app/facades/loc_result_facade.rb
+++ app/facades/loc_result_facade.rb
@@ -5,4 +5,9 @@ class LocResultFacade
       LocIndexResult.new(result)
     end
   end
+
+  def self.create_single_item_result(location, result_index_id, item_id)
+    json = LocService.get_single_location_item_data(location, result_index_id, item_id)
+    LocItemResult.new(json)
+  end
 end
