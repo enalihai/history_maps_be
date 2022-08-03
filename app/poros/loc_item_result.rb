25e700bd90b97253f9b65a153525f629ee317bfe
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-03 10:08:12 -0600

    Add details to Item results

--- app/poros/loc_item_result.rb
+++ app/poros/loc_item_result.rb
@@ -16,10 +16,18 @@ class LocItemResult
     @location_id = data[:item][:id]
     @lat = data[:item][:place][0][:latitude]
     @long = data[:item][:place][0][:longitude]
-    @details = data[:item][:notes][0][:note]
+    @details = all_details(data)
 
     @photo_url = data[:resources][0][:url]
     @photo_medium = data[:resources][0][:medium]
     @photo_large = data[:resources][0][:large]
+  end
+
+  def all_details(data)
+    details = []
+    data[:item][:notes].each do |note|
+      details << note
+    end
+    details
   end
 end
