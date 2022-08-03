fa4f8cc65bff881dadb8423c72db36bef1cd4e86
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-03 09:46:05 -0600

    Add data fields to Index poro

--- app/poros/loc_index_result.rb
+++ app/poros/loc_index_result.rb
@@ -2,13 +2,23 @@ class LocIndexResult
 
   attr_reader :title,
               :collection_link,
-              :id,
-              :index_id
+              :full_id,
+              :short_id,
+              :image_full,
+              :image_thumbnail,
+              :index_id,
+              :creator,
+              :published_date
 
   def initialize(data)
     @title = data[:title]
     @collection_link = data[:links][:item]
-    @id = data[:pk]
+    @full_id = data[:pk]
+    @short_id = @full_id[0..5]
     @index_id = data[:index]
+    @image_full = data[:image][:full]
+    @image_thumbnail = data[:image][:thumb]
+    @creator = data[:creator]
+    @published_date = data[:created_published_date]
   end
 end
