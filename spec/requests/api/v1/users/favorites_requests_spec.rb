03702351a4a4a8f2693b373c571c68f29dfeac9f
LucasPascale <lpluke5@gmail.com>
2022-08-04 01:19:49 -0500

    spec/favorites: Update attribute testing for favorites

--- spec/requests/api/v1/users/favorites_requests_spec.rb
+++ spec/requests/api/v1/users/favorites_requests_spec.rb
@@ -102,35 +102,16 @@ RSpec.describe "Favorites API requests" do
      end
   end
 
-  xit "sends one favorite of a user from a show endpoint" do
+  it "sends one favorite of a user from a show endpoint" do
     user = create(:user)
-    fav_1 = Favorite.create!( { title: "123 Real Street, Denver, CO",
-                                user_id: user.id,
-                                other_titles: "More Cool Names",
-                                pdf: "pdf_path_1",
-                                photo: "photo_path_1",
-                                details: "Some Cool notes about the thing",
-                                location_id: "coXXXX" } )
-    fav_2 = Favorite.create!( { title: "456 Real Street, Denver, CO",
-                                user_id: user.id,
-                                other_titles: "Some More Cool Names",
-                                pdf: "pdf_path_2",
-                                photo: "photo_path_2",
-                                details: "Cool notes about the thing",
-                                location_id: "coYYYY" } )
-    fav_3 = Favorite.create!( { title: "789 Real Street, Denver, CO",
-                                user_id: user.id,
-                                other_titles: "Even More Cool Names",
-                                pdf: "pdf_path_3",
-                                photo: "photo_path_4",
-                                details: "More Cool notes about the thing",
-                                location_id: "coZZZZ" } )
-
-    get "/api/v1/users/#{user.id}/favorites/#{fav_1.id}"
-
+    create_list(:favorite, 6, user_id: user.id)
+    fav_id = user.favorites.first.id
+    
+    get "/api/v1/users/#{user.id}/favorites/#{fav_id}"
+    
     response_body = JSON.parse(response.body, symbolize_names: true)
     favorite = response_body[:data]
-
+    
     expect(response).to be_successful
     expect(favorite).to be_a Hash
 
@@ -144,27 +125,36 @@ RSpec.describe "Favorites API requests" do
     expect(favorite).to have_key :attributes
     expect(favorite[:attributes]).to be_a Hash
 
+    expect(favorite).to have_key :id
+    expect(favorite[:id]).to be_a String
+
+    expect(favorite).to have_key :type
+    expect(favorite[:type]).to be_a String
+    expect(favorite[:type]).to eq 'favorite'
+
+    expect(favorite).to have_key :attributes
+    expect(favorite[:attributes]).to be_a Hash
+
     expect(favorite[:attributes]).to have_key :user_id
     expect(favorite[:attributes][:user_id]).to be_a Integer
-    expect(favorite[:attributes][:user_id]).to eq(user.id)
-
-    expect(favorite[:attributes]).to have_key :location_id
-    expect(favorite[:attributes][:location_id]).to be_a String
 
     expect(favorite[:attributes]).to have_key :title
     expect(favorite[:attributes][:title]).to be_a String
 
-    expect(favorite[:attributes]).to have_key :other_titles
-    expect(favorite[:attributes][:other_titles]).to be_a String
+    expect(favorite[:attributes]).to have_key :image_full
+    expect(favorite[:attributes][:image_full]).to be_a String
 
-    expect(favorite[:attributes]).to have_key :details
-    expect(favorite[:attributes][:details]).to be_a String
+    expect(favorite[:attributes]).to have_key :short_id
+    expect(favorite[:attributes][:short_id]).to be_a String
 
-    expect(favorite[:attributes]).to have_key :photo
-    expect(favorite[:attributes][:photo]).to be_a String
+    expect(favorite[:attributes]).to have_key :published_date
+    expect(favorite[:attributes][:published_date]).to be_a String
 
-    expect(favorite[:attributes]).to have_key :pdf
-    expect(favorite[:attributes][:pdf]).to be_a String
+    expect(favorite[:attributes]).to have_key :creator
+    expect(favorite[:attributes][:creator]).to be_a String
+
+    expect(favorite[:attributes]).to have_key :thumbnail
+    expect(favorite[:attributes][:thumbnail]).to be_a String
   end
 
   xit "sends an error if a favorite is not found" do
