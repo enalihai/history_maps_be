b6ce9b0959e60a3670c485e6eb9a68e6257188d3
LucasPascale <lpluke5@gmail.com>
2022-08-04 01:49:10 -0500

    spec/naming: Reformat names of tests for better recognition

--- spec/requests/api/v1/users/favorites_requests_spec.rb
+++ spec/requests/api/v1/users/favorites_requests_spec.rb
@@ -1,74 +1,131 @@
 require 'rails_helper'
 
-RSpec.describe "Favorites API requests" do
-  it "can create a favorite" do
-    user = create(:user)
-    fav_params = {  title: "123 Real Street, Denver, CO",
-                    image_full: "img_path_string",
-                    short_id: "short_id_path",
-                    published_date: "some_date",
-                    creator: "some_creator",
-                    thumbnail: "some_thumbnail_path" }
+RSpec.describe "API/V1/Users/user_id" do
+  describe 'Requests' do
+    it "POST /favorites :: creates a favorite" do
+      user = create(:user)
+      fav_params = {  title: "123 Real Street, Denver, CO",
+                      image_full: "img_path_string",
+                      short_id: "short_id_path",
+                      published_date: "some_date",
+                      creator: "some_creator",
+                      thumbnail: "some_thumbnail_path" }
 
-    headers = { "CONTENT_TYPE" => "application/json" }
+      headers = { "CONTENT_TYPE" => "application/json" }
 
-    post "/api/v1/users/#{user.id}/favorites", headers: headers, params: JSON.generate(fav_params)
+      post "/api/v1/users/#{user.id}/favorites", headers: headers, params: JSON.generate(fav_params)
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    
-    favorites = response_body[:data]
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      
+      favorites = response_body[:data]
 
-    expect(response).to be_successful
-    expect(response.status).to eq 201
-    expect(favorites).to be_a Hash
+      expect(response).to be_successful
+      expect(response.status).to eq 201
+      expect(favorites).to be_a Hash
 
-    expect(favorites).to have_key :id
-    expect(favorites[:id]).to be_a String
+      expect(favorites).to have_key :id
+      expect(favorites[:id]).to be_a String
 
-    expect(favorites).to have_key :type
-    expect(favorites[:type]).to be_a String
-    expect(favorites[:type]).to eq 'favorite'
+      expect(favorites).to have_key :type
+      expect(favorites[:type]).to be_a String
+      expect(favorites[:type]).to eq 'favorite'
 
-    expect(favorites).to have_key :attributes
-    expect(favorites[:attributes]).to be_a Hash
+      expect(favorites).to have_key :attributes
+      expect(favorites[:attributes]).to be_a Hash
 
-    expect(favorites[:attributes]).to have_key :user_id
-    expect(favorites[:attributes][:user_id]).to be_a Integer
-    expect(favorites[:attributes][:user_id]).to eq(user.id)
+      expect(favorites[:attributes]).to have_key :user_id
+      expect(favorites[:attributes][:user_id]).to be_a Integer
+      expect(favorites[:attributes][:user_id]).to eq(user.id)
 
-    expect(favorites[:attributes]).to have_key :title
-    expect(favorites[:attributes][:title]).to be_a String
+      expect(favorites[:attributes]).to have_key :title
+      expect(favorites[:attributes][:title]).to be_a String
 
-    expect(favorites[:attributes]).to have_key :image_full
-    expect(favorites[:attributes][:image_full]).to be_a String
+      expect(favorites[:attributes]).to have_key :image_full
+      expect(favorites[:attributes][:image_full]).to be_a String
 
-    expect(favorites[:attributes]).to have_key :short_id
-    expect(favorites[:attributes][:short_id]).to be_a String
+      expect(favorites[:attributes]).to have_key :short_id
+      expect(favorites[:attributes][:short_id]).to be_a String
 
-    expect(favorites[:attributes]).to have_key :published_date
-    expect(favorites[:attributes][:published_date]).to be_a String
+      expect(favorites[:attributes]).to have_key :published_date
+      expect(favorites[:attributes][:published_date]).to be_a String
 
-    expect(favorites[:attributes]).to have_key :creator
-    expect(favorites[:attributes][:creator]).to be_a String
+      expect(favorites[:attributes]).to have_key :creator
+      expect(favorites[:attributes][:creator]).to be_a String
 
-    expect(favorites[:attributes]).to have_key :thumbnail
-    expect(favorites[:attributes][:thumbnail]).to be_a String
-  end
+      expect(favorites[:attributes]).to have_key :thumbnail
+      expect(favorites[:attributes][:thumbnail]).to be_a String
+    end
 
-  it "sends all favorites of a user from an index endpoint" do
-    user = create(:user)
-    create_list(:favorite, 6, user_id: user.id)
+    it "GET /favorites :: returns a specific users favorites index" do
+      user = create(:user)
+      create_list(:favorite, 6, user_id: user.id)
 
-    get "/api/v1/users/#{user.id}/favorites"
+      get "/api/v1/users/#{user.id}/favorites"
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    
-    favorites = response_body[:data]
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      
+      favorites = response_body[:data]
 
-    expect(response).to be_successful
-    expect(favorites).to be_a Array
+      expect(response).to be_successful
+      expect(favorites).to be_a Array
 
-    favorites.each do |favorite|
+      favorites.each do |favorite|
+        expect(favorite).to have_key :id
+        expect(favorite[:id]).to be_a String
+
+        expect(favorite).to have_key :type
+        expect(favorite[:type]).to be_a String
+        expect(favorite[:type]).to eq 'favorite'
+
+        expect(favorite).to have_key :attributes
+        expect(favorite[:attributes]).to be_a Hash
+
+        expect(favorite[:attributes]).to have_key :user_id
+        expect(favorite[:attributes][:user_id]).to be_a Integer
+
+        expect(favorite[:attributes]).to have_key :title
+        expect(favorite[:attributes][:title]).to be_a String
+
+        expect(favorite[:attributes]).to have_key :image_full
+        expect(favorite[:attributes][:image_full]).to be_a String
+
+        expect(favorite[:attributes]).to have_key :short_id
+        expect(favorite[:attributes][:short_id]).to be_a String
+
+        expect(favorite[:attributes]).to have_key :published_date
+        expect(favorite[:attributes][:published_date]).to be_a String
+
+        expect(favorite[:attributes]).to have_key :creator
+        expect(favorite[:attributes][:creator]).to be_a String
+
+        expect(favorite[:attributes]).to have_key :thumbnail
+        expect(favorite[:attributes][:thumbnail]).to be_a String
+      end
+    end
+
+    it "GET /favorites/fav_id :: returns a specific favorite of a user" do
+      user = create(:user)
+      create_list(:favorite, 6, user_id: user.id)
+      fav_id = user.favorites.first.id
+
+      get "/api/v1/users/#{user.id}/favorites/#{fav_id}"
+      
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      favorite = response_body[:data]
+      
+      expect(response).to be_successful
+      expect(favorite).to be_a Hash
+
+      expect(favorite).to have_key :id
+      expect(favorite[:id]).to be_a String
+
+      expect(favorite).to have_key :type
+      expect(favorite[:type]).to be_a String
+      expect(favorite[:type]).to eq 'favorite'
+
+      expect(favorite).to have_key :attributes
+      expect(favorite[:attributes]).to be_a Hash
+
       expect(favorite).to have_key :id
       expect(favorite[:id]).to be_a String
 
@@ -99,127 +156,21 @@ RSpec.describe "Favorites API requests" do
 
       expect(favorite[:attributes]).to have_key :thumbnail
       expect(favorite[:attributes][:thumbnail]).to be_a String
-     end
+    end
   end
 
-  it "sends one favorite of a user from a show endpoint" do
-    user = create(:user)
-    create_list(:favorite, 6, user_id: user.id)
-    fav_id = user.favorites.first.id
+  describe 'EDGECASE::SADPATH' do
+    it "GET /favorites/fav_id :: No favorite found by ID" do
+      user = create(:user)
+      user_2 = create(:user)
+      create_list(:favorite, 3, user_id: user.id)
+      create_list(:favorite, 6, user_id: user_2.id)
 
-    get "/api/v1/users/#{user.id}/favorites/#{fav_id}"
-    
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    favorite = response_body[:data]
-    
-    expect(response).to be_successful
-    expect(favorite).to be_a Hash
-
-    expect(favorite).to have_key :id
-    expect(favorite[:id]).to be_a String
-
-    expect(favorite).to have_key :type
-    expect(favorite[:type]).to be_a String
-    expect(favorite[:type]).to eq 'favorite'
-
-    expect(favorite).to have_key :attributes
-    expect(favorite[:attributes]).to be_a Hash
-
-    expect(favorite).to have_key :id
-    expect(favorite[:id]).to be_a String
-
-    expect(favorite).to have_key :type
-    expect(favorite[:type]).to be_a String
-    expect(favorite[:type]).to eq 'favorite'
-
-    expect(favorite).to have_key :attributes
-    expect(favorite[:attributes]).to be_a Hash
-
-    expect(favorite[:attributes]).to have_key :user_id
-    expect(favorite[:attributes][:user_id]).to be_a Integer
-
-    expect(favorite[:attributes]).to have_key :title
-    expect(favorite[:attributes][:title]).to be_a String
-
-    expect(favorite[:attributes]).to have_key :image_full
-    expect(favorite[:attributes][:image_full]).to be_a String
-
-    expect(favorite[:attributes]).to have_key :short_id
-    expect(favorite[:attributes][:short_id]).to be_a String
-
-    expect(favorite[:attributes]).to have_key :published_date
-    expect(favorite[:attributes][:published_date]).to be_a String
-
-    expect(favorite[:attributes]).to have_key :creator
-    expect(favorite[:attributes][:creator]).to be_a String
-
-    expect(favorite[:attributes]).to have_key :thumbnail
-    expect(favorite[:attributes][:thumbnail]).to be_a String
-  end
-
-  it "sends an error if a favorite is not found" do
-    user = create(:user)
-    user_2 = create(:user)
-    create_list(:favorite, 3, user_id: user.id)
-    create_list(:favorite, 6, user_id: user_2.id)
-
-    get "/api/v1/users/#{user.id}/favorites/4"
-    
-    expect(response.status).to eq(404)
-    expect(response.code).to eq("404")
-    expect(response.message).to eq("Not Found")
-  end
-
-  xit "can create a favorite" do
-    user = create(:user)
-    fav_params = {  title: "123 Real Street, Denver, CO",
-                    user_id: user.id,
-                    other_titles: "More Cool Names",
-                    pdf: "pdf_path",
-                    photo: "photo_path",
-                    details: "Cool notes about the thing",
-                    location_id: "coXXXX" }
-
-    headers = { "CONTENT_TYPE" => "application/json" }
-
-    post "/api/v1/users/#{user.id}/favorites", headers: headers, params: JSON.generate(fav_params)
-
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    favorites = response_body[:data]
-
-    expect(response).to be_successful
-    expect(response.status).to eq 201
-    expect(favorites).to be_a Hash
-
-    expect(favorites).to have_key :id
-    expect(favorites[:id]).to be_a String
-
-    expect(favorites).to have_key :type
-    expect(favorites[:type]).to be_a String
-    expect(favorites[:type]).to eq 'favorite'
-
-    expect(favorites).to have_key :attributes
-    expect(favorites[:attributes]).to be_a Hash
-
-    expect(favorites[:attributes]).to have_key :user_id
-    expect(favorites[:attributes][:user_id]).to be_a Integer
-
-    expect(favorites[:attributes]).to have_key :location_id
-    expect(favorites[:attributes][:location_id]).to be_a String
-
-    expect(favorites[:attributes]).to have_key :title
-    expect(favorites[:attributes][:title]).to be_a String
-
-    expect(favorites[:attributes]).to have_key :other_titles
-    expect(favorites[:attributes][:other_titles]).to be_a String
-
-    expect(favorites[:attributes]).to have_key :details
-    expect(favorites[:attributes][:details]).to be_a String
-
-    expect(favorites[:attributes]).to have_key :photo
-    expect(favorites[:attributes][:photo]).to be_a String
-
-    expect(favorites[:attributes]).to have_key :pdf
-    expect(favorites[:attributes][:pdf]).to be_a String
+      get "/api/v1/users/#{user.id}/favorites/4"
+      
+      expect(response.status).to eq(404)
+      expect(response.code).to eq("404")
+      expect(response.message).to eq("Not Found")
+    end
   end
 end
--- spec/requests/api/v1/users/users_requests_spec.rb
+++ spec/requests/api/v1/users/users_requests_spec.rb
@@ -1,48 +1,78 @@
 require 'rails_helper'
 
-RSpec.describe "Users API Requests" do
-  it "can create a user" do
-    user_params = { username: 'bigboy',
-                    email: 'suchemail@email.com' }
+RSpec.describe "API/V1/Users" do
+  describe 'Requests' do
+    it "POST /register :: creates a user" do
+      user_params = { username: 'bigboy',
+                      email: 'suchemail@email.com' }
 
-    headers = { "CONTENT_TYPE" => "application/json" }
+      headers = { "CONTENT_TYPE" => "application/json" }
 
-    post "/api/v1/users/register", headers: headers, params: JSON.generate(user_params)
+      post "/api/v1/users/register", headers: headers, params: JSON.generate(user_params)
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    user = response_body[:data]
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      user = response_body[:data]
 
-    expect(response).to be_successful
-    expect(response.status).to eq 201
-    expect(user).to be_a Hash
+      expect(response).to be_successful
+      expect(response.status).to eq 201
+      expect(user).to be_a Hash
 
-    expect(user).to have_key :id
-    expect(user[:id]).to be_a String
+      expect(user).to have_key :id
+      expect(user[:id]).to be_a String
 
-    expect(user).to have_key :type
-    expect(user[:type]).to be_a String
-    expect(user[:type]).to eq 'user'
+      expect(user).to have_key :type
+      expect(user[:type]).to be_a String
+      expect(user[:type]).to eq 'user'
 
-    expect(user).to have_key :attributes
-    expect(user[:attributes]).to be_a Hash
+      expect(user).to have_key :attributes
+      expect(user[:attributes]).to be_a Hash
 
-    expect(user[:attributes]).to have_key :email
-    expect(user[:attributes]).to have_key :username
-    expect(user[:attributes][:email]).to be_a String
-    expect(user[:attributes][:username]).to be_a String
-  end
+      expect(user[:attributes]).to have_key :email
+      expect(user[:attributes]).to have_key :username
+      expect(user[:attributes][:email]).to be_a String
+      expect(user[:attributes][:username]).to be_a String
+    end
 
-  it "send all users from an index endpoint" do
-    create_list(:user, 5)
-    get '/api/v1/users'
+    it "GET /users :: returns all users" do
+      create_list(:user, 5)
+      get '/api/v1/users'
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    users = response_body[:data]
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      users = response_body[:data]
 
-    expect(response).to be_successful
-    expect(users).to be_a Array
+      expect(response).to be_successful
+      expect(users).to be_a Array
 
-    users.each do |user|
+      users.each do |user|
+        expect(user).to have_key :id
+        expect(user[:id]).to be_a String
+
+        expect(user).to have_key :type
+        expect(user[:type]).to be_a String
+        expect(user[:type]).to eq('user')
+
+        expect(user).to have_key :attributes
+        expect(user[:attributes]).to be_a Hash
+
+        expect(user[:attributes]).to have_key :username
+        expect(user[:attributes][:email]).to be_a String
+
+        expect(user[:attributes]).to have_key :email
+        expect(user[:attributes][:username]).to be_a String
+      end
+    end
+
+    it "GET /user_id :: returns a specific user" do
+      user_1 = create(:user)
+
+      get "/api/v1/users/#{user_1.id}"
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      user = response_body[:data]
+
+      expect(response).to be_successful
+      expect(user).to be_a Hash
+
       expect(user).to have_key :id
       expect(user[:id]).to be_a String
 
@@ -53,82 +83,25 @@ RSpec.describe "Users API Requests" do
       expect(user).to have_key :attributes
       expect(user[:attributes]).to be_a Hash
 
-      expect(user[:attributes]).to have_key :username
+      expect(user[:attributes]).to have_key :email
       expect(user[:attributes][:email]).to be_a String
 
-      expect(user[:attributes]).to have_key :email
+      expect(user[:attributes]).to have_key :username
       expect(user[:attributes][:username]).to be_a String
-     end
+    end
   end
 
-  it "send one user from a show endpoint" do
-    user_1 = create(:user)
+  describe 'EDGECASE::SADPATH' do
+    it "GET /users/user_id ::  No user is found" do
+      user = User.create({ email: 'fakeemail@email.com',
+                          username: 'RealUser',
+                          id: 1 })
 
-    get "/api/v1/users/#{user_1.id}"
+      get "/api/v1/users/2"
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    user = response_body[:data]
-
-    expect(response).to be_successful
-    expect(user).to be_a Hash
-
-    expect(user).to have_key :id
-    expect(user[:id]).to be_a String
-
-    expect(user).to have_key :type
-    expect(user[:type]).to be_a String
-    expect(user[:type]).to eq('user')
-
-    expect(user).to have_key :attributes
-    expect(user[:attributes]).to be_a Hash
-
-    expect(user[:attributes]).to have_key :email
-    expect(user[:attributes][:email]).to be_a String
-
-    expect(user[:attributes]).to have_key :username
-    expect(user[:attributes][:username]).to be_a String
-  end
-
-  it "can create a user" do
-    user_params = { username: 'bigboy',
-                    email: 'suchemail@email.com' }
-
-    headers = { "CONTENT_TYPE" => "application/json" }
-
-    post "/api/v1/users/register", headers: headers, params: JSON.generate(user_params)
-
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    user = response_body[:data]
-
-    expect(response).to be_successful
-    expect(response.status).to eq 201
-    expect(user).to be_a Hash
-
-    expect(user).to have_key :id
-    expect(user[:id]).to be_a String
-
-    expect(user).to have_key :type
-    expect(user[:type]).to be_a String
-    expect(user[:type]).to eq 'user'
-
-    expect(user).to have_key :attributes
-    expect(user[:attributes]).to be_a Hash
-
-    expect(user[:attributes]).to have_key :email
-    expect(user[:attributes]).to have_key :username
-    expect(user[:attributes][:email]).to be_a String
-    expect(user[:attributes][:username]).to be_a String
-  end
-
-  it "sends an error if a favorite is not found" do
-    user = User.create({ email: 'fakeemail@email.com',
-                         username: 'RealUser',
-                         id: 1 })
-
-    get "/api/v1/users/2"
-
-    expect(response.status).to eq(404)
-    expect(response.code).to eq("404")
-    expect(response.message).to eq("Not Found")
+      expect(response.status).to eq(404)
+      expect(response.code).to eq("404")
+      expect(response.message).to eq("Not Found")
+    end
   end
 end
